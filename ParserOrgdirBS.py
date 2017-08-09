1b58b4f4856e3ea17315428cf44233101bb326c1
Алексей <tihonovalexey29@gmail.com>
2017-08-09 14:46:42 +0300

    six_commit

--- ParserOrgdirBS.py
+++ ParserOrgdirBS.py
@@ -24,24 +24,34 @@ def next_page(HOME_PAGE_URL):
     next_link = []
     category_list = [tag.text for tag in soup.select('.h5 > a')]
     for link in category_list:
-        next_link_page = next_link.append(HOME_PAGE_URL + '/' + link + '/')
+        next_link.append(HOME_PAGE_URL + '/' + link + '/')
     return (next_link)
 
 #print(next_page(HOME_PAGE_URL))
 
+# возвращает ссылки на фирмы
 for link in next_page(HOME_PAGE_URL):
     #time(sleep(3))
     response = requests.get(link)
     soup = BeautifulSoup(response.text, 'lxml')
     firm_list = [tag.text for tag in soup.select('.h4 > a')]
     firm_links = [link.get('href') for link in soup.select('.h4 > a')]
-    #print(firm_links)
+    # print(firm_links) #https://kostroma.orgdir.ru/pribrejnyy-morskaya-c70000001017392507/?h=cliEA9G6G481G5GAH2H18rl7ggm3A52587831130juvqp9G45390I1A6B4602pyeBp8p671I0GA12029H878G5I4GJ2GGf6
 
+def firm_page(firm_links):
 
-def get_firm_info(firm_links):
-    response = requests.get(firm_links)
-    soup = BeautifulSoup(response.text, 'lxml')
-    for info in soup:
-        site = [tag.text for tag in soup.find_all('span', itemprop="url")]
+    for link in firm_links:
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, 'lxml')
+        header = [tag.text for tag in soup.find_all('h1')]
 
-    print(site)
+    return header
+
+# def get_firm_info(firm_links):
+#     response = requests.get(firm_links)
+#     soup = BeautifulSoup(response.text, 'lxml')
+#     header = [tag.text for tag in soup.find_all('h1')]
+#     site = [tag.text for tag in soup.find_all('span', itemprop="url")]
+#     email = [tag.text for tag in soup.find_all('span', itemprop="email")]
+#
+#     print(site)
